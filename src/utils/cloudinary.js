7383abdc5decee7c874ebf752ca45a784afda4c6
Abhishek Oza <101032683+Ahishekoza@users.noreply.github.com>
2024-03-14 23:18:14 +0530

    cloudinary credentials added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  if (!localFilePath) return null;   // if file path is not specified return null
+
+
+  await cloudinary.uploader.upload(localFilePath,{resource_type:'auto'})
+  .then((response)=>{
+    //  response contains many things   but we need repsonse.url
+    console.log(response);
+    fs.unlinkSync(localFilePath)
+    return response;
+  })
+  .catch((error)=>{
+    console.log(error);
+    fs.unlinkSync(localFilePath)
+    return null 
+  })
+};
+
+
+export {uploadOnCloudinary}
